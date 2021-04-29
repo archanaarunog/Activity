a521e3a66076fcad61f6a3f970687e387809cc6a
archanaarunog <archanaarunog@gmail.com>
2021-04-30 02:38:31 +0530

    main func

--- src/Activity1.c
+++ src/Activity1.c
@@ -1,4 +1,10 @@
 #include <avr/io.h>
+#define Switch_press (!(PIND & (1<<PD2)) && !(PIND &(1<<PD3)) )
+
+/**
+ * @brief Initialising the pins 
+ * 
+ */
 void init_port()
 {
     DDRB |=(1<<PB0); // set output pin for led Port b pin 0
@@ -7,14 +13,18 @@ void init_port()
     DDRD&=~(1<<PD3); //clear bit for input pin heater
     PORTD|=(1<<PD3); //set bit at high
 }
-
+/**
+ * @brief Checking whether both heater and seat button is ON
+ * 
+ * @return int return 1 if both buttons are pressed
+ */
 int Activity1(void)
 {
     init_port();
        while(1)
     {
 
-       if(!(PIND&(1<<PD2))&&!(PIND&(1<<PD3))) //switch press)
+       if(Switch_press)) //switch press)
         {
         PORTB|=(1<<PB0);
         return 1;
